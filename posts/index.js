c0a07ab31a9fb2cda62e898c9036777af263bd47
Francis Gyimah <fgyimah883@gmail.com>
2020-08-21 13:48:53 +0000

    Created the post service

---
+++ posts/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const posts = {};
+
+app.get('/posts', (_req, res) => {
+  res.json(posts);
+});
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  post[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).json(post[id]);
+});
+
+app.listen(4000, () => {
+  console.log('Posts server listening on port 4000');
+});
