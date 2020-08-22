d32d74df95ffe968aa995bfb708b18f6305c3d72
Francis Gyimah <fgyimah883@gmail.com>
2020-08-22 11:47:54 +0000

    added an event bus implementation with express

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { randomBytes } = require('crypto');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
@@ -12,7 +13,7 @@ const commentsByPostId = {};
 app.get('/posts/:id/comments', (req, res) => {
   res.json(commentsByPostId[req.params.id] || []);
 });
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
@@ -21,7 +22,22 @@ app.post('/posts/:id/comments', (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
   res.status(201).json(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received event', req.body.type);
+
+  res.send({ status: 'OK' });
 });
 
 app.listen(4001, () => {
