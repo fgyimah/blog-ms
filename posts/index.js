1e4c7addc783f99516bdddac035a2c7efafed146
Francis Gyimah <fgyimah883@gmail.com>
2020-08-23 22:51:15 +0000

    Final ingress setup

--- posts/index.js
+++ posts/index.js
@@ -13,7 +13,7 @@ const posts = {};
 app.get('/posts', (_req, res) => {
   res.json(posts);
 });
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
